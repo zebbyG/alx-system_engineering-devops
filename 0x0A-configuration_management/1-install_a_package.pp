4108c566c5a58c410f1a9ebf5ade6f2e83396766
email <zebbyG>
2022-09-16 13:20:28 +0300

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask from pip3
-exec { 'flask':
-  provider => 'pip3',
-  command  => '/usr/bin/apt-get -y install flask -v 2.1.0',
+# Using Puppet to install flask from pip3.
+exec { 'install python packages':
+    command => 'pip3 install flask flask_restful apiai',
+    path    => ['/usr/bin/'],
+    unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
 }
