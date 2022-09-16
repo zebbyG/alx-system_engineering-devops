2f48515de96ae584ff84696c2bcbab854ed901ef
email <zebbyG>
2022-09-16 12:50:40 +0300

    try task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # install flask from pip3
-package { 'flask':
-  ensure => 'installed',
-  source => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
